3c4fad0d843cccc79ca7764dfc9d4cb84226bd59
Dheeraj Garg <dheerajgarg.in@gmail.com>
2022-06-20 18:41:05 +0530

    Deploying

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mgk9i#3b_)w+if3$&!1t!1$qopj1iuv-$bnx$%)(+hqhi=@ycz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 # Application definition
 
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- studybuddy/urls.py
+++ studybuddy/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import url
 from django.urls import path
 from django.urls.conf import include
 from dashboard import views as dash_views
@@ -26,5 +28,8 @@ urlpatterns = [
     path('register/', dash_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name = "dashboard/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = "dashboard/logout.html"), name='logout'),
-    path('profile/', dash_views.profile, name='profile')
+    path('profile/', dash_views.profile, name='profile'),
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}), 
 ]
